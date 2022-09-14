b7a95efcd2e9340fcc18231df0f6b47f1429715c
Thiago Sandeiro <102810162@student.swin.edu.au>
2022-09-14 18:01:22 +1000

    Update home.php

--- assets/web_layer/home.php
+++ assets/web_layer/home.php
@@ -27,31 +27,77 @@
             <!-- Section 1 -->
             <section id="manage">
                 <h1 id="h1">Home Page</h1>
-                <hr />
+                <hr /><br />
 
                 <!-- Form -->
-                <div id="manage-container">
-                  <form id='LoginForm' class='form' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>' method='post' novalidate='novalidate'>
-                    <?php require 'login.php';?>
+                <div id="manage-employee">
+                  <form id='manage-employee-form' class='form' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>' method='post' novalidate='novalidate'>
+                    <?php require './login.inc';?>
                     <section>
 
-                      <!-- Admin Details -->
-                      <fieldset id='admin-details'>
-                        <legend>Employee Login Details</legend>
+                      <!-- Employee Details -->
+                      <fieldset>
+                        <legend>Employee Details</legend>
 
-                        <label for='email'>Email</label>
-                          <input type='text' name='email' placeholder='Email...' /><br /><br />
+                        <label for='employeeName'>Name</label>
+                          <input type='text' name='employeeName' placeholder='Employee Name...' /><br /><br />
 
-                        <label for='password'>Password</label>
-                          <input type='password' name='password' placeholder='Password...' /><br /><br />
+                        <label for='employeeAddress'>Address</label>
+                          <input type='text' name='employeeAddress' placeholder='Employee Address...' /><br /><br />
 
+                        <label for='employeeEmail'>Email</label>
+                          <input type='text' name='employeeEmail' placeholder='Employee Email...' /><br /><br />
+
+                        <label for='employeePassword'>Password</label>
+                          <input type='password' name='employeePassword' placeholder='Employee Password...' /><br /><br />
+
+                        <label for='employeeCRUD'></label>
+                          <input type='radio' name='employeeCRUD' value='create' />Add
+                          <input type='radio' name='employeeCRUD' value='read' />Display
+                          <input type='radio' name='employeeCRUD' value='update' />Update
+                          <input type='radio' name='employeeCRUD' value='delete' />Delete<br /><br />
+
+                          <button form='manage-employee-form' type='submit'>Submit</button>
                       </fieldset>
-                      <button form='LoginForm' id='Create-admin' class='btn' type='submit'>Login</button>
-                      <br />
                       <br />
                     </section>
                   </form>
                 </div>
+
+                <hr /><br />
+
+                <!-- Form -->
+                <div id="manage-member">
+                  <form id='manage-member-form' class='form' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>' method='post' novalidate='novalidate'>
+                    <?php require './login.inc';?>
+                    <section>
+
+                      <!-- Member Details -->
+                      <fieldset>
+                        <legend>Member Details</legend>
+
+                        <label for='memberName'>Name</label>
+                          <input type='text' name='memberName' placeholder='Member Name...' /><br /><br />
+
+                        <label for='memberAddress'>Address</label>
+                          <input type='text' name='memberAddress' placeholder='Member Address...' /><br /><br />
+
+                        <label for='memberEmail'>Email</label>
+                          <input type='text' name='memberEmail' placeholder='Member Email...' /><br /><br />
+
+                        <label for='memberCRUD'></label>
+                          <input type='radio' name='memberCRUD' value='create' />Add
+                          <input type='radio' name='memberCRUD' value='read' />Display
+                          <input type='radio' name='memberCRUD' value='update' />Update
+                          <input type='radio' name='memberCRUD' value='delete' />Delete<br /><br />
+
+                          <button form='manage-member-form' type='submit'>Submit</button>
+                      </fieldset>
+                      <br />
+                    </section>
+                  </form>
+                </div>
+
             </section>
         </article>
     </main>
